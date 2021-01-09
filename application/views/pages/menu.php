e6201c62076516e22e828b06446cf57c30388f7c
ahmedkrdzalic <ahmed.krdzalic@gmail.com>
2021-01-09 16:21:55 +0100

    Update menu.php

--- application/views/pages/menu.php
+++ application/views/pages/menu.php
@@ -1,5 +1,35 @@
 <div class="menu_body">
-		
+	<div class="margin_menu"></div>
+	<div class="menu_container_menu">
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men1.jpg" class="icons">
+		</div>	
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men2.jpg" class="icons">
+		</div>	
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men3.jpg" class="icons">
+		</div>	
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men4.jpg" class="icons">
+		</div>	
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men5.jpg" class="icons">
+		</div>
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men6.jpg" class="icons">
+		</div>	
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men7.jpg" class="icons">
+		</div>	
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men8.jpg" class="icons">
+		</div>
+		<div class="card_menu">
+			<img src="<?php echo base_url(); ?>assets/images/menu/men9.jpg" class="icons">
+		</div>
+	</div>
+	<div class="margin_menu"></div>
 </div>
 
 
